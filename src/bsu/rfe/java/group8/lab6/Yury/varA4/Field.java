9271add9dbab4581de76b4bd97bdebdf28e2be0d
YuryAvdeenko <avdeenko.yura@mail.ru>
2021-03-31 23:54:30 +0300

    Add functional in class Field

--- src/bsu/rfe/java/group8/lab6/Yury/varA4/Field.java
+++ src/bsu/rfe/java/group8/lab6/Yury/varA4/Field.java
@@ -43,5 +43,36 @@ public class Field extends JPanel {
             ball.paint(canvas);
         }
     }
-
+    // Метод добавления нового мяча в список
+    public void addBall() {
+//Заключается в добавлении в список нового экземпляра BouncingBall
+// Всю инициализацию положения, скорости, размера, цвета
+// BouncingBall выполняет сам в конструкторе
+        balls.add(new BouncingBall(this));
+    }
+    // Метод синхронизированный, т.е. только один поток может
+// одновременно быть внутри
+    public synchronized void pause() {
+// Включить режим паузы
+        paused = true;
+    }
+    // Метод синхронизированный, т.е. только один поток может
+// одновременно быть внутри
+    public synchronized void resume() {
+// Выключить режим паузы
+        paused = false;
+// Будим все ожидающие продолжения потоки
+        notifyAll();
+    }
+    // Синхронизированный метод проверки, может ли мяч двигаться
+// (не включен ли режим паузы?)
+    public synchronized void canMove(BouncingBall ball) throws
+            InterruptedException {
+        if (paused) {
+// Если режим паузы включен, то поток, зашедший
+// внутрь данного метода, засыпает
+            wait();
+        }
+    }
 }
+
